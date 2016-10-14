993712ca7dfb0e03bb62965bb3c11eaa7ee688c3
SebastianDotNet_cp <>
2016-10-15 00:51:59 +0200

    [Regular Commit]
    - fix issue #22037 - Excel.GetActiveInstances
    - fix issue #21998 - CustomUIAttribute
    - fix issue #21959 - InlineResponseClose DispID
    - improve Excel=>Names.Item (optionals)
    - fix PowerPoint _Application.HWND (instead of Microsoft)
    - fix a lot of compiler warnings in main source
    - add TimeSpan COMAddinBase.LoadingTimeElapsed{get;}
    - add TimeSpan Core.InitializedTime{get;}
    - add Settings.EnableUnknownProxies{get;set;}
    - add Concept Tests\ProxyView application
    - improve COMAddin Tweaks
    - improve COMAddin utils again
    - add Lockback, Programmable, Codebase attribute
    - Add RegistrySaveLocation=>InstallScope
    - recreate addin Register/Unregister logic
    - delete/move a lot of duplicate code in different
      COMAddin base classes to 1 central point
    - add Deployment\RegAddin application
    - update office 2016 support meta informations
    - update assemblies

---
+++ Tests/Concept Tests/GetRunningInstances/ConsoleApplication1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Excel = NetOffice.ExcelApi;
+using PowerPoint = NetOffice.PowerPointApi;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetExcelActiveInstances();
+            GetActivePowerPointInstance();
+            Console.WriteLine("{0}Press any key...", Environment.NewLine);
+            Console.ReadKey();
+        }
+
+        static void GetExcelActiveInstances()
+        {
+            Console.WriteLine("NetOffice Concept Test - Excel.Application.GetActiveInstances(){0}", Environment.NewLine);
+            Excel.Application[] apps = Excel.Application.GetActiveInstances();
+
+            Console.WriteLine("{0} active Excel instance(s) found.{1}", apps.Length, Environment.NewLine);
+
+            foreach (Excel.Application app in Excel.Application.GetActiveInstances())
+            {
+                string workbooks = string.Empty;
+                foreach (Excel.Workbook openBook in app.Workbooks)
+                    workbooks += openBook.Name + " ";
+
+                Console.WriteLine("Excel.Application {0} has open workbooks:{1}", app.Hwnd, workbooks);
+                app.Dispose();
+            }
+        }
+
+        static void GetActivePowerPointInstance()
+        {
+            PowerPoint.Application application = null;
+            try
+            {
+                NetOffice.Settings.Default.UseExceptionMessage = NetOffice.ExceptionMessageHandling.CopyInnerExceptionMessageToTopLevelException;
+                Console.WriteLine("NetOffice Concept Test - PowerPoint.Application.GetActiveInstance(){0}", Environment.NewLine);
+                application = PowerPoint.Application.GetActiveInstance(false);
+                if (null != application)
+                {
+                    Console.WriteLine("Current PowerPoint Application Visibility: {0}", application.Visible);
+                }
+                else 
+                {
+                    Console.WriteLine("No PowerPoint Application running.");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("An error has occured. {0}", exception.Message);
+            }
+            finally
+            {
+                if (null != application)
+                {
+                    application.Dispose();
+                    application = null;
+                }
+            }
+        }
+    }
+}
